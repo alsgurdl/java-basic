0c9c2ef0cca37e4f786c303b0abcf3731c65ab75
kmh <fdf98952@gmail.com>
2024-03-06 18:01:19 +0900

    3/6 18

--- src/etc/api/io/stream/StreamCopy.java
+++ src/etc/api/io/stream/StreamCopy.java
@@ -1,31 +1,28 @@
-//package etc.api.io.stream;
-//
-//import com.sun.source.tree.WhileLoopTree;
-//
-//import java.io.FileInputStream;
-//import java.io.FileOutputStream;
-//
-//public class StreamCopy {
-//    public static void main(String[] args) {
-//        try(
-//                FileOutputStream fos = new FileOutputStream("C:/my/cet.jpg");
-//                FileInputStream fis = new FileInputStream("C:/my/copy.jpg"){
-//
-//
-//                }
-//        ) {
-//            int data =0;
-//            while (data != -1){
-//                data =fis.read();
-//                fos.write(data);
-//            }
-//
-//        } catch(Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//
-//    }
-//
-//}
+package etc.api.io.stream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class StreamCopy {
+
+    public static void main(String[] args) {
+
+        try(FileOutputStream fos = new FileOutputStream("C:/MyWorkspace/copy.png");
+            FileInputStream fis = new FileInputStream("C:/MyWorkspace/chicken.png")) {
+
+            int data = 0;
+            while(data != -1) {
+                // 1byte 단위로 데이터를 읽어들임과 동시에 데이터를 작성.
+                data = fis.read();
+                fos.write(data);
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
